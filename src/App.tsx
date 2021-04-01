feec9d1f985d7d7103d8862b5876a1022fc1d290
Jonathan Rupp <jrupp@rightpoint.com>
2021-04-01 09:33:29 -0500

    Import from lodash normally

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { cloneDeep } from 'lodash';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <span>{ cloneDeep({}) }</span>
         <a
           className="App-link"
           href="https://reactjs.org"
